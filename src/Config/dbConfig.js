029e3c32c832a14299adc99dfead33b7e1c207fa
danyulloa21 <ulloadaniel2101@gmail.com>
2024-11-24 02:47:46 -0700

    se corrigio la desconexion por falta de actividad, seguimos probando a ver si funciona el metodo , veremos despues en un tiempo

--- src/Config/dbConfig.js
+++ src/Config/dbConfig.js
@@ -4,20 +4,36 @@ const mysql = require('mysql2');
 const dbConfig = {
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: '=Pulparindo123_',
   database: 'departamentos',
   multipleStatements: true, // Habilita el soporte para múltiples consultas
 };
 
-const connection = mysql.createConnection(dbConfig);
+const handleDisconnect = () => {
+  const connection = mysql.createConnection(dbConfig);
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error de conexión a la base de datos:', err);
-  } else {
-    console.log('Conexión exitosa a la base de datos');
-  }
-});
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error al reconectar a la base de datos:', err);
+      setTimeout(handleDisconnect, 2000); // Reintentar después de 2 segundos
+    } else {
+      console.log('Reconexión exitosa a la base de datos');
+    }
+  });
+
+  connection.on('error', (err) => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('Conexión cerrada. Intentando reconectar...');
+      handleDisconnect(); // Reconectar si la conexión se pierde
+    } else {
+      throw err;
+    }
+  });
+
+  return connection;
+};
+
+const connection = handleDisconnect();
 
 module.exports = connection;
  
